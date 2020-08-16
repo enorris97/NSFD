c539b910d947b12751e76d5d81dcaede5b0f41e6
Eleanor Norris <36667902+enorris97@users.noreply.github.com>
2020-08-16 15:38:07 +0100

    clean build

--- target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.mycompany.nsfd","l":"cmdTesting"},{"p":"com.mycompany.nsfd","l":"disclaimer"},{"p":"com.mycompany.nsfd","l":"mainJFrame"},{"p":"com.mycompany.nsfd","l":"resultsJFrame"},{"p":"com.mycompany.nsfd","l":"startJFrame"},{"p":"com.mycompany.nsfd","l":"StartProgram"},{"p":"com.mycompany.nsfd","l":"test"}]
